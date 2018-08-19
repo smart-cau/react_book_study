413bfb9c5d3df080304e1b2f44638453ccaba2ef
HyeonWoo <1995hyunwoo@naver.com>
2018-08-19 19:56:43 +0900

    chapter 5 ref  ___ before use ref

---
+++ src/ValidationSample.js
@@ -0,0 +1,46 @@
+// Chapter 5. ref사용하기.
+import React, { Component } from "react";
+import "./ValidationSample.css";
+
+class ValidationSample extends Component {
+  state = {
+    password: "",
+    clicked: false,
+    validated: false
+  };
+
+  handleChange = e => {
+    this.setState({
+      password: e.target.value
+    });
+  };
+
+  handleButtonClick = () => {
+    this.setState({
+      clicked: true,
+      validated: this.state.password === "0000" //이 등식이 성립하면 true, 그렇지 않으면 false를 return.
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          className={
+            this.state.clicked
+              ? this.state.validated
+                ? "success"
+                : "failure"
+              : ""
+          }
+        />
+        <button onClick={this.handleButtonClick}>검증하기</button>
+      </div>
+    );
+  }
+}
+
+export default ValidationSample;
